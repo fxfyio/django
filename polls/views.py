09d1825f9f9a7d10e44f11e4bf1672bdeaab0492
fxfyio <fxfyio@pm.me>
2023-08-25 17:43:52 +0800

    update

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.urls import reverse
 
 
 def index(request):
@@ -12,10 +13,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
@@ -24,5 +27,24 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, question_id):  # 定义投票视图，需要request对象和问题ID作为参数
+    question = get_object_or_404(
+        Question, pk=question_id)  # 尝试获取问题对象，如果没有找到，返回404错误
+
+    try:
+        selected_choice = question.choice_set.get(
+            pk=request.POST["choice"])  # 尝试获取选中的选项，使用POST中的“choice”值
+    except (KeyError, Choice.DoesNotExist):  # 如果未找到选项或未选择选项，则捕获异常
+        return render(  # 返回详细页面，并显示错误消息
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice."
+            },
+        )
+    else:
+        selected_choice.votes += 1  # 如果找到选项，则增加投票数
+        selected_choice.save()  # 保存选项对象，以更新数据库中的投票数
+        # 重定向到结果页面
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
